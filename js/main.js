fb9f87a672dbfcf88027a277c1c4ef6ccb46b429
Gavin Barnett <gavbarnett@gmail.com>
2019-02-16 15:58:43 +0000

    adding nodejs server for API

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@
  */
 
 function main() {
-   
+   args = window.location.hrecof.split('&')
+   document.write(args)
     if(window.location.hrecof.indexOf("lat") != -1){
         //this is not very robust
         var id = window.location.href.substr(window.location.href.indexOf("lat")+9)
