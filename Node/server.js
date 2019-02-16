f1c037ea88a0d0c9634243e013a42b525249e113
Gavin Barnett <gavbarnett@gmail.com>
2019-02-16 22:56:20 +0000

    getting ready to be useful
    cant find a historic weather api thats free to use / or uses open data. :-1:

--- Node/server.js
+++ Node/server.js
@@ -1,14 +1,86 @@
 var restify = require('restify');
+var test_mode = true //false
+var restify = require('restify-clients');
+var API_key_metoffice = "9a55e44f-72b8-47b6-8968-67967d63cb76"
+//http://datapoint.metoffice.gov.uk/public/data/val/wxobs/all/datatype/sitelist?key=9a55e44f-72b8-47b6-8968-67967d63cb76
+//aberdeen = 3091
+// Creates a JSON client
+var client = restify.createJsonClient({
+  url: 'http://history.openweathermap.org'
+});
 
-function respond(req, res, next) {
-  res.send('hello ' + req.params.name);
-  next();
+//client.basicAuth('$login', '$password');
+client.get('/my/machines', function(err, req, res, obj) {
+  assert.ifError(err);
+
+  console.log(JSON.stringify(obj, null, 2));
+});
+
+function location_resolver(id){
+    //add look up to database for lat/lon
+    lat = 0
+    lon = 0
+    switch(id){
+        case 5331:
+            lat = 57.128
+            lon = -2.130
+            break
+        case 7789:
+            lat = 57.130
+            lon = -2.087
+            break
+        case 8554:
+            lat = 57.146
+            lon = -2.114
+            break
+        case 8733:
+            lat = 57.136
+            long = -2.107
+            break
+    }
+    return ([lat, lon])
 }
 
-var server = restify.createServer();
-server.get('/hello/:name', respond);
-server.head('/hello/:name', respond);
+function weather(req, res, next) {
+    if (test_mode) {console.log('reponded to request for weather')}
+    jsondata = location_resolver(req.params.id)
+    //jsondata = weather_agrigator()
+
+    var options = {
+        host: url,
+        port: 80,
+        path: '/resource?id=foo&bar=baz',
+        method: 'POST'
+      };
+      
+      http.request(options, function(res) {
+        console.log('STATUS: ' + res.statusCode);
+        console.log('HEADERS: ' + JSON.stringify(res.headers));
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+          console.log('BODY: ' + chunk);
+        });
+      }).end()
+
+    res.send(jsondata)
+    next();
+}
+
+function help(req, res, next) {
+    if (test_mode) {console.log('reponded to request for help')}
+    jsondata = {
+        "help":"API options",
+        "weather":{"id":"device id", "start_date":"unix time, UTC time zone", "end_date":"unix time, UTC time zone"}
+    }
+    res.send(jsondata)
+    next();
+}
+
+var server = restify.createServer()
+//server.get('/traffic/:id/:start_date/:end_date/:interval', traffic);
+server.get('/weather', weather); ///:id/:start_date/:end_date/:interval
+server.get('/help', help);
 
 server.listen(8080, function() {
-  console.log('%s listening at %s', server.name, server.url);
+    console.log('%s listening at %s', server.name, server.url);
 });
