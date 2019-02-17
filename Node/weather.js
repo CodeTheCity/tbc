1671d5017d10020851747f7a59f5735e8aafeca3
Gavin Barnett <gavbarnett@gmail.com>
2019-02-17 12:35:25 +0000

    api returns 2days of hourly data from a end_date at a given sensor id

--- Node/weather.js
+++ Node/weather.js
@@ -7,7 +7,12 @@ exports.simple = function(req, res, next){
     var request = require('request');
     var data = ""
     LatLon = locationjs.idToLatLon(req)
-    API_CALL = 'https://api.darksky.net/forecast/' + key + '/' + LatLon[0] +',' + LatLon[1]
+//    var time = 255657600
+//    var time = Math.round(new Date("2017-09-15 00:00:00.000").getTime()/1000);
+    var time = Math.round(new Date(req.query.end_date).getTime()/1000);
+    //https://api.darksky.net/forecast/[key]/[latitude],[longitude],[time]
+    var url = 'https://api.darksky.net/forecast/' + key + '/'
+    API_CALL = url + LatLon[0] +',' + LatLon[1] + ',' + time + '?exlude=currently, minutely, daily, alerts, flags'
     request(API_CALL, function (error, response, body) {
         if (!error && response.statusCode == 200) {
         data = JSON.parse(body)
